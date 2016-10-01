c529f7d608cfd4da68e144ed5728af093c1375e7
Emanuel Rosu <rosu.emanuel@gmail.com>
2016-09-30 22:43:40 -0700

    Changed to owned content distribution

--- content.js
+++ content.js
@@ -28,12 +28,12 @@ chrome.runtime.onMessage.addListener
     (function(request, sender, sendResponse) {
       if( request.message === "sent_upload_request" ) {
         var iframe = document.createElement("iframe");
-        iframe.setAttribute("src", "https://www.voki.com/images/ajax-loader.gif");
+        iframe.setAttribute("src", "https://dl.dropboxusercontent.com/u/29669245/ajax-loader.gif");
         iframe.setAttribute("style", "border:none; width:400px; height:400px; position:fixed; top: 50%; left: 50%; margin-top: -200px; margin-left:-200px;");
         iframe.setAttribute("scrolling", "no");
         iframe.setAttribute("frameborder", "0");
         document.body.appendChild(iframe);
-        fadeIn(iframe, 300);
+        fadeIn(iframe, 700);
     }
   }
 );
@@ -47,12 +47,12 @@ chrome.runtime.onMessage.addListener
         }
 
         var iframe = document.createElement("iframe");
-        iframe.setAttribute("src", "https://summericeworld.files.wordpress.com/2015/06/smiley-face-clip-art-thumbs-up-clipart-two-thumbs-up-happy-smiley-emoticon-512x512-eec6.png?w=593&h=159");
+        iframe.setAttribute("src", "https://dl.dropboxusercontent.com/u/29669245/smiley.png");
         iframe.setAttribute("style", "border:none; width:600px; height:500px; position:fixed; top: 50%; left: 50%; margin-top: -250px; margin-left:-300px;");
         iframe.setAttribute("scrolling", "no");
         iframe.setAttribute("frameborder", "0");
         document.body.appendChild(iframe);
-        fadeIn(iframe, 300);
+        fadeIn(iframe, 700);
 
         setTimeout(function() {
             iframes = document.querySelectorAll('iframe');
